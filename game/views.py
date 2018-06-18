4cf72fd7506a1de2a1fde2ff114edaa484aefc64
Jamie Yeh <jamie.yeh@disney.com>
2018-06-18 14:54:47 -0700

    adds create page

--- game/views.py
+++ game/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Game
+from .forms import GameForm
 
 def home(request):
-    return render(request, 'home.html', {'title': 'testing'})
+    context = {
+    'title':'Welcome To Game Rater',
+    'games': Game.objects.order_by('-votes'),
+    }
+    return render(request, 'home.html', context)
 
+
+@login_required
 def create(request):
-
-    game_form = GameForm()
+    if request.method == 'POST':
+        game_form = GameForm(data=request.POST)
+        if game_form.is_valid():
+            game = game_form.save(commit=False)
+            # We'll set commit=False so we can populate our model with non model form data
+            game.game_rater = request.user
+            # Now we can insert the logged in user, they were attached to the request body
+            game.save()
+            return redirect('home')
+    else:
+        game_form = GameForm()
 
     return render(request, 'create.html', {
-    'title':'Add a game to be Voted On',
-    'game_form': game_form
+    'title':'Add a game to be voted on',
+    'game_form': game_form,
     })
