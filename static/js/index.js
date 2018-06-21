1b496340ba9e22f8632355b2f782d6553a0243a3
Jamie Yeh <jamie.yeh@disney.com>
2018-06-21 10:42:32 -0700

    looping running guys working

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,16 @@
 $(function () {
 
+    function run(){
+      guys.css('right', startPos);
+      guys.animate({right: -1270}, 15000, 'linear')
+    };
 
+     var screenWidth = $(document).width();
+     var startPos = screenWidth;
+     var guys = $('#guys');
+     run();
+     setInterval(function() {
+       run();
+     }, 16000);
 
 });
